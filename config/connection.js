fbc119ea1781d0ff7e6d6ae4b6b44544b7706f95
THandley19 <trevor.handley@gmail.com>
2020-03-27 23:07:06 -0400

    fixing fatal error

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,26 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "burgers_db"
-    });
-};
+  connection = mysql.createConnection({
+    connectionLimit: 100,
+    waitForConnections: true,
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+    wait_timeout: 28800,
+    connect_timeout: 10
+  });
+}
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("listening on localhost:" + connection.threadId);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("listening on localhost:" + connection.threadId);
 });
 
 module.exports = connection;
